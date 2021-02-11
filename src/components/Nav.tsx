f72494d5cba309e4bfcf7c0fc60e573cbb63f4c3
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-11 16:13:56 -0600

    start styling nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Flex, Spacer, Link } from '@chakra-ui/react';
+import { Flex, Spacer, Link, Stack, Box } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 
 export const Nav = () => {
   return (
-    <Flex direction='row'>
-      <Link as={RouterLink} to='/'>
-        Home
-      </Link>
-    </Flex>
+    <Box bg='gray.800' color='white' p={2} boxShadow='lg'>
+      <Flex mx='auto' justify='space-between' direction='row' maxW='200px'>
+        <Link as={RouterLink} to='/'>
+          Home
+        </Link>
+        <Link as={RouterLink} to='/form'>
+          Form
+        </Link>
+        <Link as={RouterLink} to='/card'>
+          Card
+        </Link>
+        <Link as={RouterLink} to='/list'>
+          List
+        </Link>
+      </Flex>
+    </Box>
   );
 };
