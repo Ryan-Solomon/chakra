fdf92032942c0e318fcc55370d3f286bc42b44b1
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-11 17:37:39 -0600

    setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import { Nav } from './components/Nav';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
 
 export const App = () => {
   return (
     <>
       <Nav />
+      <Switch>
+        <Route exact path='/'>
+          <HomePage />
+        </Route>
+        <Route exact path='/form'>
+          <FormPage />
+        </Route>
+      </Switch>
     </>
   );
 };
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const HomePage = () => {
+  return (
+    <>
+      <h2>Ryan's Blog</h2>
+    </>
+  );
+};
