8270487b2865ccd4715179341f968bbcbaca649a
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-11 16:33:44 -0600

    layout nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
-import { Flex, Spacer, Link, Stack, Box } from '@chakra-ui/react';
+import { Flex, Spacer, Link, Stack, Box, HStack } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
+import { BsLightningFill } from 'react-icons/bs';
 
 export const Nav = () => {
   return (
-    <Box bg='gray.800' color='white' p={2} boxShadow='lg'>
-      <Flex mx='auto' justify='space-between' direction='row' maxW='200px'>
-        <Link as={RouterLink} to='/'>
-          Home
-        </Link>
-        <Link as={RouterLink} to='/form'>
-          Form
-        </Link>
-        <Link as={RouterLink} to='/card'>
-          Card
-        </Link>
-        <Link as={RouterLink} to='/list'>
-          List
-        </Link>
-      </Flex>
+    <Box
+      display='flex'
+      justifyContent='center'
+      as='nav'
+      bg='gray.800'
+      color='white'
+      p={2}
+      boxShadow='lg'
+    >
+      <HStack mx='auto' justify='center' spacing={40}>
+        <Box>
+          <BsLightningFill color='white' />
+        </Box>
+        <Flex
+          as='ul'
+          fontSize='lg'
+          mx='auto'
+          justify='space-between'
+          direction='row'
+          align='center'
+          width='200px'
+        >
+          <Link as={RouterLink} to='/'>
+            Home
+          </Link>
+          <Link as={RouterLink} to='/form'>
+            Form
+          </Link>
+          <Link as={RouterLink} to='/card'>
+            Card
+          </Link>
+          <Link as={RouterLink} to='/list'>
+            List
+          </Link>
+        </Flex>
+        <Box>
+          <BsLightningFill color='white' />
+        </Box>
+      </HStack>
     </Box>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
       <Router>
+        <CSSReset />
         <App />
       </Router>
     </ChakraProvider>
