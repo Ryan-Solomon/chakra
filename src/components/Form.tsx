3957e3973f20b2c1b357ffff20f4b646b83fedee
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-11 18:00:55 -0600

    add form inputs

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,17 +4,35 @@ import {
   FormHelperText,
   FormLabel,
   Input,
+  Stack,
 } from '@chakra-ui/react';
 import React from 'react';
 
 export const Form = () => {
   return (
     <Box maxW='50%' mx='auto' mt={20}>
-      <FormControl id='email'>
-        <FormLabel>Email address</FormLabel>
-        <Input type='email' />
-        <FormHelperText>We'll never share your email.</FormHelperText>
-      </FormControl>
+      <Stack spacing={8}>
+        <FormControl id='first-name'>
+          <FormLabel mb={3}>First Name</FormLabel>
+          <Input type='text' />
+        </FormControl>
+        <FormControl id='last-name'>
+          <FormLabel mb={3}>Last Name</FormLabel>
+          <Input type='text' />
+        </FormControl>
+        <FormControl id='email'>
+          <FormLabel mb={3}>Email address</FormLabel>
+          <Input type='email' />
+        </FormControl>
+        <FormControl id='password'>
+          <FormLabel mb={3}>Password</FormLabel>
+          <Input type='password' />
+        </FormControl>
+        <FormControl id='password'>
+          <FormLabel mb={3}>Confirm Password</FormLabel>
+          <Input type='password' />
+        </FormControl>
+      </Stack>
     </Box>
   );
 };
