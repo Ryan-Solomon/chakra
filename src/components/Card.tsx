5a3d4d03061e8340725dbaf2575ca115ecdc26d7
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-11 19:42:39 -0600

    add img and box to card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,27 @@
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
 import React from 'react';
 
 export const Card = () => {
   return (
-    <>
-      <h1>Card</h1>
-    </>
+    <Box
+      background='gray.700'
+      mx='auto'
+      mt={10}
+      width='200px'
+      borderRadius={10}
+    >
+      <Image
+        boxSize='100%'
+        objectFit='cover'
+        src='https://i.pinimg.com/originals/54/7a/9c/547a9cc6b93e10261f1dd8a8af474e03.jpg'
+        alt='Segun Adebayo'
+        borderRadius={10}
+        borderBottomRightRadius={0}
+        borderBottomLeftRadius={0}
+      />
+      <Flex>
+        <Text>Introduction to Chakra UI</Text>
+      </Flex>
+    </Box>
   );
 };
