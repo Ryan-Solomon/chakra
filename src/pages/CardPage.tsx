63cc5971ab24d2d3b007da68b2af1ccb66a422fc
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-02-11 18:14:00 -0600

    setup card component

--- src/pages/CardPage.tsx
+++ src/pages/CardPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Card } from '../components/Card';
 
 export const CardPage = () => {
   return (
     <>
-      <h1>Card</h1>
+      <Card />
     </>
   );
 };
